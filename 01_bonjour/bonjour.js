21e921625d7b7e3f406cdd98d88ff8ebea7d0913
joeldevel <christianjoel.developer@gmail.com>
2022-02-18 20:10:42 -0300

    adds first example. It's like a hello world

---
+++ 01_bonjour/bonjour.js
@@ -0,0 +1,7 @@
+/*
+ * Prints a message to the console
+ * Author: joeldevel
+ * How to run it: node bonjour.js
+ **/
+console.log("S'imaginant que la trágedie n'est autre chose que l'art de louer...");
+console.log("\t\t\tErnest Renan, Averroes, 48(1861)");
